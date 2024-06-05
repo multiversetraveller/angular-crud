a853f788970a8938430f05a891dc9ea18c733362
ANKIT DIXIT <60250868+multiversetraveller@users.noreply.github.com>
2024-06-06 01:36:34 +0530

    ankit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,8 +17,18 @@ export class HomeComponent implements OnInit {
   snapshot: any;
   getupdateimage$!: Observable<string | undefined>;
   downloadURL: any;
+  number!: number;
+  rangesData = {
+    data: [
+      { min: 200000, max: 1000000, step: 50000 },
+      { min: 1000000, max: 2000000, step: 100000 },
+      { min: 2000000, max: 4000000, step: 200000 },
+      { min: 4000000, max: 8600000, step: 500000 },
+    ],
+    baseValue: 200000
+  };
 
-  constructor( 
+   constructor( 
     public authservices:AuthServiceService,
     public afsmode:AngularFireStorage,    public afstore: AngularFirestore,private firestore:Firestore,
 
@@ -26,7 +36,8 @@ export class HomeComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.getAllVipMembers()
+    this.getAllVipMembers();
+    this.number = this.rangesData.baseValue; // Initialize number to baseValue
   }
 
   upload(event:any ) {
@@ -56,4 +67,43 @@ export class HomeComponent implements OnInit {
      })
      
    } 
+
+  
+
+  getStep() {
+    for (const range of this.rangesData.data) {
+      if (this.number >= range.min && this.number < range.max) {
+        return range.step;
+      }
+    }
+    return this.rangesData.data[0].step; // Default step
+  }
+
+  increment() {
+    const step = this.getStep();
+    const currentRange = this.rangesData.data.find(range => this.number >= range.min && this.number < range.max);
+
+    if (currentRange && (this.number + step) < currentRange.max) {
+      this.number += step;
+    } else {
+      const nextRange = this.rangesData.data.find(range => this.number < range.min);
+      if (nextRange) {
+        this.number = nextRange.min;
+      }
+    }
+  }
+
+  decrement() {
+    const step = this.getStep();
+    const currentRange = this.rangesData.data.find(range => this.number >= range.min && this.number < range.max);
+
+    if (currentRange && (this.number - step) >= currentRange.min) {
+      this.number -= step;
+    } else {
+      const prevRange = this.rangesData.data.slice().reverse().find(range => this.number >= range.max);
+      if (prevRange) {
+        this.number = prevRange.max - prevRange.step;
+      }
+    }
+  }
 }
